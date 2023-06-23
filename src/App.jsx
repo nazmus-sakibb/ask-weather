e6c6e63a212185b3205e98d0956ad649b6178ee6
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-24 01:51:48 +0600

    get OpenWeatherMap Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=25aea6110b7561ce2e7bd38ff9924d60`
+
 
   return (
-    <>
-      
-    </>
+    <div>
+      <div className="container">
+        <div className="top">
+          <div className="locatin">
+            
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
   )
 }
 
