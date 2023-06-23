33babff8e1a52151eb24ca04d48f3876ad195117
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-24 02:39:07 +0600

    made basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,33 @@ function App() {
 
 
   return (
-    <div>
+    <div className='app'>
       <div className="container">
         <div className="top">
-          <div className="locatin">
-            
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>32°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>38°F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className='bold'>20 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
       </div>
     </div>
   )
